be824e7218d8081231379f1873ded740e9e83709
Hendrik Schuster <h.schuster@deviantdev.com>
2017-06-18 16:52:45 +0200

    Extend the windows installer to find the idea.jar itself
    The batch file now searches for the idea.jar file in the default JetBrains Directory. Multplie versions are handled well.

--- install.bat
+++ install.bat
@@ -1,5 +1,7 @@
 @echo off
- 
+color 17
+mode con: cols=90 lines=25
+
 ::Windows XP doesn't have UAC so skip
 for /f "tokens=3*" %%i in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName ^| Find "ProductName"') do set WINVER=%%i %%j 
 echo %WINVER% | find "XP" > nul && goto commands
@@ -25,14 +27,43 @@ del /q "%TEMP%\UAC.vbs"
 ::navigate back to this script's home folder
 %~d2
 cd "%~p2"
- 
-::main script here
-set idea_lib_path="C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2017.1.4\lib"
-set idea_jar_path="C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2017.1.4\lib\idea.jar"
 
-jar -ufv %idea_jar_path% com/intellij/ide/ui/laf/darcula/darcula.properties
-jar -ufv %idea_jar_path% com/intellij/ide/ui/laf/intellijlaf.properties
+::
+:: Main script executed after elevation
+::
 
-ROBOCOPY . %idea_lib_path% "idea.jar" /NJS /NJH
-
+echo -----------------------------------------------------------------------------------------
+echo.
+echo --------------------------- Solarized UI Theme for IntelliJ -----------------------------
+echo.
+echo -----------------------------------------------------------------------------------------
+:: search the default JetBrains directory for the idea.jar file - there can be multiple
+set searchDirectory="C:\Program Files\JetBrains"
+set searchFile=idea.jar
+for /d /r %searchDirectory% %%v in (%searchFile%) do if exist "%%~fv" (
+    call :install "%%~fv"
+  )
+echo.
+echo -----------------------------------------------------------------------------------------
+echo.
+echo -----------------------------------------------------------------------------------------
+echo.
+echo  Installation completed! Your IntelliJ is now solarized.
 pause
+exit 0
+
+:install
+echo.
+echo   Would you like to install the solarized UI theme for the following version?
+echo.
+echo   %1
+echo.
+set /p choice="> Type [Y]es or [N]o: " 
+if /i "%choice:~0,1%"=="Y" (
+  echo.
+  jar -ufv %1 com/intellij/ide/ui/laf/darcula/darcula.properties
+  jar -ufv %1 com/intellij/ide/ui/laf/intellijlaf.properties
+)
+echo.
+echo -----------------------------------------------------------------------------------------
+goto :eof
